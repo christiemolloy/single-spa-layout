97abacd0ea5e037ce447059dda51e87ac41f42dc
Christie Molloy <cmolloy@redhat.com>
2020-07-29 17:05:00 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,64 +1,11 @@
 import React from 'react';
 import './App.scss';
-// import {
-//   Nav,
-//   NavItem,
-//   Page,
-//   PageHeader,
-//   PageHeaderTools,
-//   PageSidebar,
-//   PageSection,
-//   PageSectionVariants,
-// } from '@patternfly/react-core';
 
 import {
   NavLink,
 } from 'react-router-dom';
 
 function App() {
-  // const [isNavOpen, setIsNavOpen] = useState(false);
-  // const [isActiveItem, setIsActiveItem] = useState(0);
-
-  // const onNavToggle = () => {
-  //   setIsNavOpen(!isNavOpen);
-  // };
-
-  // const onSelect = (result) => {
-  //   setIsActiveItem(result.itemId);
-  // };
-
-  // const logoProps = {
-  //   href: 'https://patternfly.org',
-  //   target: '_blank',
-  // };
-
-  // const Header = (
-  //   <PageHeader
-  //     logo="Logo"
-  //     logoProps={logoProps}
-  //     showNavToggle
-  //     isNavOpen={isNavOpen}
-  //     onNavToggle={onNavToggle}
-  //   />
-  // );
-
-  // const PageNav = (
-  //   <Nav onSelect={onSelect} aria-label="Nav">
-  //     <NavItem itemId={0} isActive={isActiveItem === 0}>
-  //       <Link key="0" to="/react-app-one">
-  //         React app one
-  //       </Link>
-  //     </NavItem>
-  //     <NavItem itemId={1} isActive={isActiveItem === 1}>
-  //       <Link key="1" to="/react-app-two">
-  //         React app two
-  //       </Link>
-  //     </NavItem>
-  //   </Nav>
-  // );
-
-  // const Sidebar = <PageSidebar nav={PageNav} isNavOpen={isNavOpen} />;
-
   return (
     <React.Fragment>
       <div id="container">
@@ -70,10 +17,6 @@ function App() {
           <h1>Red Hat singla-spa example application.</h1>
         </div>
       </div>
-      {/* <Switch>
-        <Route exact path="/" component={Services} />
-        <Route path="/tools" component={Tools} />
-      </Switch> */}
     </React.Fragment>
   );
 }
