1b2ec7d0d3e82b146e84919c8e41821ab93ccb2a
Christie Molloy <cmolloy@redhat.com>
2020-07-29 16:09:23 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,66 +1,80 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
+// import {
+//   Nav,
+//   NavItem,
+//   Page,
+//   PageHeader,
+//   PageHeaderTools,
+//   PageSidebar,
+//   PageSection,
+//   PageSectionVariants,
+// } from '@patternfly/react-core';
+
 import {
-  Nav,
-  NavItem,
-  Page,
-  PageHeader,
-  PageHeaderTools,
-  PageSidebar,
-  PageSection,
-  PageSectionVariants,
-} from '@patternfly/react-core';
-import { Link } from 'react-router-dom';
+  NavLink,
+} from 'react-router-dom';
 
 function App() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isActiveItem, setIsActiveItem] = useState(0);
+  // const [isNavOpen, setIsNavOpen] = useState(false);
+  // const [isActiveItem, setIsActiveItem] = useState(0);
 
-  const onNavToggle = () => {
-    setIsNavOpen(!isNavOpen);
-  };
+  // const onNavToggle = () => {
+  //   setIsNavOpen(!isNavOpen);
+  // };
 
-  const onSelect = (result) => {
-    setIsActiveItem(result.itemId);
-  };
+  // const onSelect = (result) => {
+  //   setIsActiveItem(result.itemId);
+  // };
 
-  const logoProps = {
-    href: 'https://patternfly.org',
-    target: '_blank',
-  };
+  // const logoProps = {
+  //   href: 'https://patternfly.org',
+  //   target: '_blank',
+  // };
 
-  const Header = (
-    <PageHeader
-      logo="Logo"
-      logoProps={logoProps}
-      headerTools={<PageHeaderTools>header-tools</PageHeaderTools>}
-      showNavToggle
-      isNavOpen={isNavOpen}
-      onNavToggle={onNavToggle}
-    />
-  );
+  // const Header = (
+  //   <PageHeader
+  //     logo="Logo"
+  //     logoProps={logoProps}
+  //     showNavToggle
+  //     isNavOpen={isNavOpen}
+  //     onNavToggle={onNavToggle}
+  //   />
+  // );
 
-  const PageNav = (
-    <Nav onSelect={onSelect} aria-label="Nav">
-      <NavItem itemId={0} isActive={isActiveItem === 0}>
-        <Link key="0" to="/react-app-one">
-          React app one
-        </Link>
-      </NavItem>
-      <NavItem itemId={1} isActive={isActiveItem === 1}>
-        <Link key="1" to="/react-app-two">
-          React app two
-        </Link>
-      </NavItem>
-    </Nav>
-  );
+  // const PageNav = (
+  //   <Nav onSelect={onSelect} aria-label="Nav">
+  //     <NavItem itemId={0} isActive={isActiveItem === 0}>
+  //       <Link key="0" to="/react-app-one">
+  //         React app one
+  //       </Link>
+  //     </NavItem>
+  //     <NavItem itemId={1} isActive={isActiveItem === 1}>
+  //       <Link key="1" to="/react-app-two">
+  //         React app two
+  //       </Link>
+  //     </NavItem>
+  //   </Nav>
+  // );
 
-  const Sidebar = <PageSidebar nav={PageNav} isNavOpen={isNavOpen} />;
+  // const Sidebar = <PageSidebar nav={PageNav} isNavOpen={isNavOpen} />;
 
   return (
-    <Page header={Header} sidebar={Sidebar}>
-      <PageSection variant={PageSectionVariants.darker}>Section with darker background</PageSection>
-    </Page>
+    <React.Fragment>
+      <div id="container">
+        <nav id="links">
+          <NavLink to="/react-app-one" activeClassName="active" exact="true">Red Hat Fuse</NavLink>
+          <NavLink to="/react-app-two" activeClassName="active" exact="true">Red Hat AMQ</NavLink>
+        </nav>
+        <div id="main">
+          <h1>Red Hat singla-spa example application.</h1>
+        </div>
+      </div>
+      {/* <Switch>
+        <Route exact path="/" component={Services} />
+        <Route path="/tools" component={Tools} />
+      </Switch> */}
+    </React.Fragment>
   );
 }
 
